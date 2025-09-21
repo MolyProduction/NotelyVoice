e5f4b67c4f74be8a53a5546488dc2cc84b6c1ff6
Tosin <tosinmath007@gmail.com>
2025-09-21 14:27:56 +0100

    App version update to 1.2.3 (24)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -165,8 +165,8 @@ android {
         applicationId = "com.module.notelycompose.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.1.2"
+        versionCode = 24
+        versionName = "1.2.3"
     }
     buildFeatures {
         compose = true
